9d212077f9235f64503fc3d85e09f9ef433e721a
Zachary Totaro <ztotaro24@biomedscienceacademy.org>
2020-11-05 15:57:32 +0000

    I added my inputs, but they may change.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 """
 MadLibs
-Author: 
-Period/Core:
-
-
+Author: Zach Totaro
+Period/Core: Core 2
 """
+person = input("Enter a name: ")
+place_one = input("Enter a place: ")
+place_two = input("Enter another place: ")
+food_one = input("Enter a food: ")
+food_two = input("Enter another food: ")
+adjective_one = input("Enter an adjective: ")
+adjective_two = input("Enter another adjective: ")
+verb = input("Enter a verb: ")
+noun = input("Enter a noun: ")
+num = int(input("Enter a number: "))
