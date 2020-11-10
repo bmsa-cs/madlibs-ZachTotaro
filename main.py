9016faeefdaff1ae3d15a48401387bb9a2bed4b7
Zachary Totaro <ztotaro24@biomedscienceacademy.org>
2020-11-10 01:02:11 +0000

    I finished up the story, and made sure everything was good to submit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ MadLibs
 Author: Zach Totaro
 Period/Core: Core 2
 """
+print("Let's play Silly Sentences!\n")
 name = input("Enter a name: ")
 place = input("Enter a place: ")
 food_one = input("Enter a food: ")
@@ -13,5 +14,6 @@ verb = input("Enter a verb: ")
 noun = input("Enter a noun: ")
 year = int(input("Enter a year: "))
 vehicle = input("Enter a vehicle: ")
+drink = input("Enter a drink: ")
 
-print(f"{name} wants to go to {place} to see all of the {adjective_one} {noun}s that {place} is famous for. While {name} is there, they hope to {verb} with all the locals. {name}'s favorite food is {food_one}, esepecially when it's {adjective_two}.They hope that the restaurant also has {food_two} to go with the{food_one}. {name} will leave for {place} tomorrow morning in his {year} {vehicle}.")
+print("\n" + name + f" wants to go to {place} to see all of the {adjective_one} {noun}s that {place} is famous for. While {name} is there, they hope to {verb} with all the locals. {name}'s favorite food is {food_one}, esepecially when it's {adjective_two}. They plan on going to a restaurant. {name} hopes that the restaurant also has {food_two} to go with the {food_one}. {name} will leave for {place} tomorrow morning in his {year} {vehicle}. {name} will have to remember to stop and get some {drink} on the way, because it's going to be a long drive.")
